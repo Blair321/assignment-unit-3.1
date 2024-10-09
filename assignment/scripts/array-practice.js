70a80669d0527db0d26e9ace52753dd9c4f9335f
Blair Jilek <blairjilek@gmail.com>
2024-10-09 16:20:43 -0500

    added a new food to fav foods called dessert  and pushed it to end of the array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -67,6 +67,9 @@ console.log('Added an animal to end,', animalArray);
 //      a dessert that you love.
 //      Similar to above, add the `dessert` to the end of your `favoriteFoods`
 //      array. (How can you be 100% certain this worked? 🤔)
+let dessert = 'apple pie';
+favoriteFoods.push(dessert);
+console.log('apple pie dessert to end',favoriteFoods);
 
 
 // Example: Add to the beginning of an array using the Array.unshift method.
